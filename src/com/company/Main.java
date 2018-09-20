2c50994c8583a5b3a6895c30bf326360d6ee28ff
Steffen Rasmussen <42993915+TheWildDane@users.noreply.github.com>
2018-09-20 11:22:53 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("J");
+        System.out.println("J  aaa    v     vaa");
+        System.out.println("J  J  aa    v v    a");
+        System.out.println("J   aaaa     v     aaa");
+    }
+}
